dc462fbcd8bea7ec8d8af226d6f1c1896770f38f
Fabio Vige <fabiovige@gmail.com>
2024-07-23 16:34:11 -0300

    Implementado GetProductById

--- cmd/main.go
+++ cmd/main.go
@@ -30,6 +30,8 @@ func main() {
 	})
 
 	server.GET("/products", ProductController.GetProducts)
+	server.POST("/products", ProductController.CreateProduct)
+	server.GET("/products/:productId", ProductController.GetProductById)
 
 	server.Run(":8000")
 }
